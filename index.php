708642badb90f8229d4b896ed72acb0874fae063
GolfSkyNet <montol@gmail.com>
2019-11-04 21:37:41 +0700

    video

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 // Token
-$channel_token = '2MCOyCeaBipmw3ZzJG8BrsiO4KzCoaoPddMgbZtEu5HHVeIaWU+PDKcCZRJEY76zqxv56d15kZeMoU/vQ0zuzPFlbhFM7AhRMZwLrSkLdciLCuKUgV6aFrvAAuuG1mMWe7DCzfEW9FfHQhJR4F/m0AdB04t89/1O/w1cDnyilFU=';
-$channel_secret = 'd4afd7da941ac195c155fe67dcb5a338';
+$channel_token = 'B5qap2QpWS3QLd19pOsh4FUhgNq7v5zhDtKrh4mLH/Lq0GWmCGJlZZ4SUfFwl7j2mdBQrdaC2EivgOGwUXQYCDeAH1L98XDOve0IWNWwUiiVYzZ9qFSXFUcDriOSISJ73QFfMS9jKz0M9WJqded67wdB04t89/1O/w1cDnyilFU=';
+$channel_secret ='63219f8490cb174ce6fd928c5c188ca0';
 // Get message from Line API
 $content = file_get_contents('php://input');
 $events = json_decode($content, true);
